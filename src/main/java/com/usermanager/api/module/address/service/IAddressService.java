cc52b6f1b71d41efa85756b4be51b0b5f45f1105
Michel_Freitas <michelfreitas85@outlook.com>
2024-06-02 14:38:12 -0300

    Removing Service layer from Address

--- src/main/java/com/usermanager/api/module/address/service/IAddressService.java
+++
@@ -1,8 +0,0 @@
-package com.usermanager.api.module.address.service;
-
-import com.usermanager.api.module.address.dto.RCreateAddressDto;
-import com.usermanager.api.module.address.model.AddressModel;
-
-public interface IAddressService {
-//    AddressModel create(RCreateAddressDto addressDto);
-}
