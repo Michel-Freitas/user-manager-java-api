c078c5c4aa81e61c22f7965fdb2aacf19ee94f7d
Michel_Freitas <michelfreitas85@outlook.com>
2024-06-05 02:19:19 -0300

    Implementing User List and Details.

--- src/main/java/com/usermanager/api/module/user/controller/UserController.java
+++ src/main/java/com/usermanager/api/module/user/controller/UserController.java
@@ -2,12 +2,16 @@ package com.usermanager.api.module.user.controller;
 
 import com.usermanager.api.module.user.dto.RCreateUserDto;
 import com.usermanager.api.module.user.dto.RUpdateUserDto;
+import com.usermanager.api.module.user.dto.RUserDetailsDto;
+import com.usermanager.api.module.user.dto.RUserDto;
 import com.usermanager.api.module.user.service.IUserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -27,4 +31,15 @@ public class UserController {
         this.userService.update(id, updateUserDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping
+    public ResponseEntity<List<RUserDto>> listAll() {
+        return new ResponseEntity<>(this.userService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RUserDetailsDto> details(@PathVariable Long id) {
+        return new ResponseEntity<>(this.userService.details(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/usermanager/api/module/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/usermanager/api/module/user/service/impl/UserServiceImpl.java
@@ -1,17 +1,25 @@
 package com.usermanager.api.module.user.service.impl;
 
+import com.usermanager.api.module.address.dto.RAddressDto;
 import com.usermanager.api.module.address.model.AddressModel;
 import com.usermanager.api.module.user.dto.RCreateUserDto;
 import com.usermanager.api.module.user.dto.RUpdateUserDto;
+import com.usermanager.api.module.user.dto.RUserDetailsDto;
+import com.usermanager.api.module.user.dto.RUserDto;
 import com.usermanager.api.module.user.exception.CpfAlreadyUsedException;
 import com.usermanager.api.module.user.exception.DifferentUserIdsException;
 import com.usermanager.api.module.user.exception.UserNotFoundException;
 import com.usermanager.api.module.user.model.UserModel;
 import com.usermanager.api.module.user.repository.IUserRepository;
 import com.usermanager.api.module.user.service.IUserService;
+import jakarta.persistence.EntityGraph;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Service
@@ -20,6 +28,8 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private IUserRepository userRepository;
 
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
     public UserModel create(RCreateUserDto createUserDto) {
@@ -49,9 +59,8 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public UserModel update(Long userId, RUpdateUserDto updateUserDto) {
-        if (!Objects.equals(userId, updateUserDto.id())) {
+        if (!Objects.equals(userId, updateUserDto.id()))
             throw new DifferentUserIdsException();
-        }
 
         UserModel user = this.userRepository.findById(userId)
                 .orElseThrow(UserNotFoundException::new);
@@ -62,5 +71,52 @@ public class UserServiceImpl implements IUserService {
         user.setStatus(updateUserDto.status());
 
         return this.userRepository.save(user);
+    }
+
+    @Override
+    public List<RUserDto> listAll() {
+        return this.userRepository.findAll().stream()
+                .map(item -> new RUserDto(
+                        item.getId(),
+                        item.getName(),
+                        item.getCpf(),
+                        item.getDateBirth(),
+                        item.getRole(),
+                        item.getStatus()))
+                .toList();
+    }
+
+    @Override
+    public RUserDetailsDto details(Long id) {
+        EntityGraph<UserModel> graph = this.entityManager.createEntityGraph(UserModel.class);
+        graph.addAttributeNodes("address");
+
+        Map<String, Object> hints = new HashMap<>();
+        hints.put("javax.persistence.fetchgraph", graph);
+
+        UserModel user = this.entityManager.find(UserModel.class, id, hints);
+        if (user == null)
+            throw new UserNotFoundException();
+
+        RAddressDto addressDto = new RAddressDto(
+                user.getAddress().getId(),
+                user.getAddress().getStreet(),
+                user.getAddress().getNumber(),
+                user.getAddress().getComplement(),
+                user.getAddress().getCity(),
+                user.getAddress().getNeighborhood(),
+                user.getAddress().getState(),
+                user.getAddress().getZipCode()
+        );
+
+        return new RUserDetailsDto(
+                user.getId(),
+                user.getName(),
+                user.getCpf(),
+                user.getDateBirth(),
+                user.getRole(),
+                user.getStatus(),
+                addressDto
+        );
     }
 }
